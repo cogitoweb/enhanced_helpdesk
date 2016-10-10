81039db0f0de0176500c06ed1048d26c36b82702
gabrielebassi <gabriele.bassi@yahoo.it>
2016-10-10 08:22:23 +0200

    Update migrations.py

--- model/migrations.py
+++ model/migrations.py
@@ -2,6 +2,16 @@ from openerp.osv import osv
 class crm_ticket_status(osv.osv):
 
     _name = "crm_helpdesk_migrate"
+
+    def _create_default_ticket_status(sel, cr, udi, ids=None, context=None):
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('1', 'Nuovo')  """)
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('2', 'Presa in carico') """)           
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('3', 'in approvazione') """)     
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('4', 'In lavorazione')  """) 
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('5', 'Consegna')  """)   
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('6', 'Completato')   """)  
+        cr.execute("""INSERT INTO  helpdesk_ticket_status(id,status_name) VALUES ('7', 'Anullato')  """)           
+
     def _migrate_from_old_satus_to_new(self, cr, uid, ids=None, context=None):
         cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 1 WHERE state = 'draft' """)
         cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 3 WHERE state = 'pending' """)
--- sql_files/default_ticket_status.sql
+++ sql_files/default_ticket_status.sql
@@ -1,14 +1,21 @@
 INSERT INTO  helpdesk_ticket_status(id,status_name)
-VALUES ('1',  'Nuovo');
+VALUES ('1',  'Nuovo')
+ON CONFLICT (id) DO NOTHING;
 INSERT INTO  helpdesk_ticket_status(id,status_name)
-VALUES ('2', 'Presa in carico');
+VALUES ('2', 'Presa in carico')
+ON CONFLICT (di) DO NOTHING;
 INSERT INTO  helpdesk_ticket_status(id,status_name)
-VALUES ('3', 'in approvazione');
+VALUES ('3', 'in approvazione')
+ON CONFLICT (id) DO NOTHING;
 INSERT INTO  helpdesk_ticket_status(id,status_name) 
-VALUES ('4', 'In lavorazione');
+VALUES ('4', 'In lavorazione')
+ON CONFLICT (id) DO NOTHING;
 INSERT INTO  helpdesk_ticket_status(id,status_name)
-VALUES ('5', 'Consegna');
+VALUES ('5', 'Consegna')
+ON CONFLICT (id) DO NOTHING;
 INSERT INTO  helpdesk_ticket_status(id,status_name)
-VALUES ('6', 'Completato');
+VALUES ('6', 'Completato')
+ON CONFLICT (id) DO NOTHING;
 INSERT INTO  helpdesk_ticket_status(id,status_name) 
-VALUES ('7', 'Anullato');  
+VALUES ('7', 'Anullato')
+ON CONFLICT (id) DO NOTHING;  
