0c4b07dd1d63ffb725ee9ed80f46d1a35424029b
2z <paolo.cazzitti@cogitoweb.it>
2016-10-21 08:58:28 +0200

    pre e post migrate

--- migrations/8.0.0.2/post-migrate.py
+++ migrations/8.0.0.2/post-migrate.py
@@ -7,12 +7,21 @@ def migrate(cr, version):
         return
     
     logger.info("Updating crm_helpdesk ticket_status_id")
+    
+    if(version == '8.0.0.1'):
+        cr.execute("select max(id) from helpdesk_ticket_status where status_code = 'new'")
+        r = cr.fetchone()
+        if(r[0] != '1'):
+            cr.execute("""delete from helpdesk_ticket_status where id = """ + r[0])
 
-    cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 1 WHERE state = 'draft' """)
-    cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 3 WHERE state = 'pending' """)
-    cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 4 WHERE state = 'open' """)
-    cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 6 WHERE state = 'done' """)
-    cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = 7 WHERE state = 'cancel' """)
+            cr.execute("SELECT setval('helpdesk_ticket_status_id_seq', (SELECT MAX(id) FROM helpdesk_ticket_status))")
+
+        cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = (select id from helpdesk_ticket_status where status_code = 'new') WHERE state = 'draft' """)
+        cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = (select id from helpdesk_ticket_status where status_code = 'app') WHERE state = 'pending' """)
+        cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = (select id from helpdesk_ticket_status where status_code = 'wrk') WHERE state = 'open' """)
+        cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = (select id from helpdesk_ticket_status where status_code = 'ok') WHERE state = 'done' """)
+        cr.execute(""" UPDATE crm_helpdesk SET ticket_status_id = (select id from helpdesk_ticket_status where status_code = 'xx') WHERE state = 'cancel' """)
+    
 
 
         
--- migrations/8.0.0.2/pre-migrate.py
+++ migrations/8.0.0.2/pre-migrate.py
@@ -6,16 +6,35 @@ def migrate(cr, version):
     if not version:
         return
     
-    cr.execute("select id from helpdesk_ticket_status where id = 1")
-    r = cr.fetchone()
-    if not r:
-        cr.execute("""INSERT INTO helpdesk_ticket_status(
-                id, create_uid, create_date, status_deadline, status_name, status_code, 
-                write_uid, write_date, status_description, status_order)
-                VALUES (1, 1, now(), null, 'New', 'new', 
-                        1, now(), 'New', 1);""")
+    if(version == '8.0.0.1'):
+        cr.execute("""CREATE TABLE helpdesk_ticket_status
+            (
+              id serial NOT NULL,
+              create_uid integer, -- Created by
+              create_date timestamp without time zone, -- Created on
+              status_deadline character varying, -- Durata
+              status_name character varying NOT NULL, -- Nome Stato
+              status_code character varying NOT NULL, -- Codice Stato
+              write_uid integer, -- Last Updated by
+              write_date timestamp without time zone, -- Last Updated on
+              status_description text, -- Descrizione
+              status_order integer, -- Ordinamento
+              CONSTRAINT helpdesk_ticket_status_pkey PRIMARY KEY (id)
+            )
+            WITH (
+              OIDS=FALSE
+            )""")
+    
+        cr.execute("select id from helpdesk_ticket_status where id = 1")
+        r = cr.fetchone()
+        if not r:
+            cr.execute("""INSERT INTO helpdesk_ticket_status(
+                    id, create_uid, create_date, status_deadline, status_name, status_code, 
+                    write_uid, write_date, status_description, status_order)
+                    VALUES (1, 1, now(), null, 'New', 'new', 
+                            1, now(), 'New', 1)""")
 
-        cr.execute("SELECT setval('helpdesk_ticket_status_id_seq', (SELECT MAX(id) FROM helpdesk_ticket_status))")
+            cr.execute("SELECT setval('helpdesk_ticket_status_id_seq', (SELECT MAX(id) FROM helpdesk_ticket_status))")
 
 
 
