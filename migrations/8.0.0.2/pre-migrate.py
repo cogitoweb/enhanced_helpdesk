a264139abe3d3aec7b161c97a05e63bf492bb7d9
2z <paolo.cazzitti@cogitoweb.it>
2016-10-21 09:22:27 +0200

    pre migr consistency

--- migrations/8.0.0.2/pre-migrate.py
+++ migrations/8.0.0.2/pre-migrate.py
@@ -7,23 +7,32 @@ def migrate(cr, version):
         return
     
     if(version == '8.0.0.1'):
-        cr.execute("""CREATE TABLE helpdesk_ticket_status
-            (
-              id serial NOT NULL,
-              create_uid integer, -- Created by
-              create_date timestamp without time zone, -- Created on
-              status_deadline character varying, -- Durata
-              status_name character varying NOT NULL, -- Nome Stato
-              status_code character varying NOT NULL, -- Codice Stato
-              write_uid integer, -- Last Updated by
-              write_date timestamp without time zone, -- Last Updated on
-              status_description text, -- Descrizione
-              status_order integer, -- Ordinamento
-              CONSTRAINT helpdesk_ticket_status_pkey PRIMARY KEY (id)
-            )
-            WITH (
-              OIDS=FALSE
-            )""")
+        
+        cr.execute("""SELECT EXISTS (
+                       SELECT 1
+                       FROM   information_schema.tables 
+                       WHERE  table_schema = 'public'
+                       AND    table_name = 'helpdesk_ticket_status'
+                    )""")
+        r = cr.fetchone()
+        if not r:
+            cr.execute("""CREATE TABLE helpdesk_ticket_status
+                (
+                  id serial NOT NULL,
+                  create_uid integer, -- Created by
+                  create_date timestamp without time zone, -- Created on
+                  status_deadline character varying, -- Durata
+                  status_name character varying NOT NULL, -- Nome Stato
+                  status_code character varying NOT NULL, -- Codice Stato
+                  write_uid integer, -- Last Updated by
+                  write_date timestamp without time zone, -- Last Updated on
+                  status_description text, -- Descrizione
+                  status_order integer, -- Ordinamento
+                  CONSTRAINT helpdesk_ticket_status_pkey PRIMARY KEY (id)
+                )
+                WITH (
+                  OIDS=FALSE
+                )""")
     
         cr.execute("select id from helpdesk_ticket_status where id = 1")
         r = cr.fetchone()
