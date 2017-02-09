8992f9f6468fd9575b3864328916c2ab63bb7a28
2z <paolo.cazzitti@cogitoweb.it>
2017-02-09 16:07:30 +0100

    fix invio mail fatt

--- model/crm_helpdesk.py
+++ model/crm_helpdesk.py
@@ -400,7 +400,7 @@ class CrmHelpdesk(models.Model):
         custom_deliver = []
         child_ids = None
         if(self.task_points > 0 and self.request_id.partner_id.parent_id):
-            child_ids = t.sudo().request_id.partner_id.parent_id.child_ids
+            child_ids = self.sudo().request_id.partner_id.parent_id.child_ids
         
         if(child_ids):
             for child in child_ids:
@@ -477,7 +477,7 @@ class CrmHelpdesk(models.Model):
             
             _logger.info("approved estimation")
             is_approved = True
-            custom_deliver.extend(add_invoice_contacts())
+            custom_deliver.extend(self.add_invoice_contacts())
 
         deadline_date = None
         deadline = None
@@ -541,7 +541,7 @@ class CrmHelpdesk(models.Model):
 
         expande = {'before_body': before_body}
         
-        custom_deliver = add_invoice_contacts()
+        custom_deliver = self.add_invoice_contacts()
         
         self.send_notification_mail('email_template_ticket_change_state', 
                                     'crm.helpdesk', 
